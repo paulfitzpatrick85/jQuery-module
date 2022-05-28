a95a37e29285f93c47aea7be4aea1bc17773609c
paulfitzpatrick85 <fitzer99@gmail.com>
2022-05-28 08:09:11 +0000

    this working

--- this1/js/script.js
+++ this1/js/script.js
@@ -1,23 +1,23 @@
+
 $(document).ready(function() {
     $(".box").on("click", function() {
-    	var classNames = $(this).attr("class").split(" ");   /*When we click on an element that has a 'box' class, this event will be fired.*/
-        $("." + classNames[1]).css("background-color", "red"); //trigger is .box, but target is 'this', but will also target the second class[1] 'this' shares
-
-        var classNames = $(this).attr("class").split(" ");
+    	/**
+    	 * When we click on an element that has
+    	 * a 'box' class, this event will be fired.
+    	 */
+    	var classNames = $(this).attr("class").split(" ");
     	var boxClass = classNames[0];
     	var className = classNames[1];
-        
     	if ($(this).css("background-color") == "rgb(255, 0, 0)") {
-    		$("." + className).css("background-color", "#000");     // If this object is already red, turn it black
+    		// If this object is already red, turn it black
+    		$("." + className).css("background-color", "#000"); 
     	} else {
-    		$("." + boxClass).css("background-color", "#000");
-    		$("." + className).css("background-color", "red");
-
-            // Else turn all elements with a box class black
+    		// Else turn all elements with a box class black
     		// and then change the color of all elements
     		// with the same class name as the clicked element
     		// to red.
+    		$("." + boxClass).css("background-color", "#000");
+    		$("." + className).css("background-color", "red");
     	}
     });
 })
-    
